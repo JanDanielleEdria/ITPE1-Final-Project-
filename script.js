cec0ff4e405c254e2d1e633d88bb85d624e1263f
JanDanielleEdria <edriajandanielle@gmail.com>
2026-04-03 07:00:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function addToCart(button) {
+     
+    button.innerHTML = "✔ Added";
+
+    
+    alert("Item added to cart!");
+
+    
+    setTimeout(() => {
+        if (button.classList.contains("cart")) {
+            button.innerHTML = "Add to Cart";
+        } else {
+            button.innerHTML = "🛒";
+        }
+    }, 1500);
+}
